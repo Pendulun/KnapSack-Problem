08a59e238315cd065850c968f23c3ea6bed758a1
Pendulun <danielcampos14@hotmail.com.br>
2021-03-27 16:27:32 -0300

    BackPack.hpp

--- include/BackPackProblem/BackPack.hpp
+++ include/BackPackProblem/BackPack.hpp
@@ -1,4 +1,19 @@
 #ifndef BACKPACK_H
 #define BACKPACK_H
+#include <list>
+#include "Item.hpp"
 
+namespace BackPackProblem{
+	class BackPack{
+	private:
+		float maxWeight;
+		std::list<Item> items;
+	public:
+		BackPack(float maxWeight);
+		float solve();
+		void setItems(std::list<Item> items);
+		float getMaxWeight();
+		std::list<Item> getItems();
+	};
+}
 #endif
