4cc20798e5ea37ffa9cf17a9516de1d45d873efc
Pendulun <danielcampos14@hotmail.com.br>
2021-03-27 16:37:17 -0300

    BackPackBTFactory.hpp

--- include/BackPackProblem/BackPackBTFactory.hpp
+++ include/BackPackProblem/BackPackBTFactory.hpp
@@ -1,4 +1,12 @@
 #ifndef BACKPACKBTFACTORY_H
 #define BACKPACKBTFACTORY_H
+#include "BackPack.hpp"
 
+namespace BackPackProblem{
+	class BackPackBTFactory : public BackPackFactory{
+	public:
+		BackPackBTFactory();
+		BackPack* createBackPack() override;
+	};
+}
 #endif
