67a4251502df9c712831c70267b445abc0f3db57
Pendulun <danielcampos14@hotmail.com.br>
2021-03-27 16:54:11 -0300

    Adding include

--- include/BackPackProblem/BackPackBBFactory.hpp
+++ include/BackPackProblem/BackPackBBFactory.hpp
@@ -1,6 +1,7 @@
 #ifndef BACKPACKBBFACTORY_H
 #define BACKPACKBBFACTORY_H
 #include "BackPack.hpp"
+#include "BackPackBB.hpp"
 
 namespace BackPackProblem{
 	class BackPackBBFactory : public BackPackFactory{
