a815560e4a89b113935ae2bf416acd6c9d2edabb
Pendulun <danielcampos14@hotmail.com.br>
2021-03-27 16:45:45 -0300

    BackPack.cpp

---
+++ src/BackPackProblem/BackPack.cpp
@@ -0,0 +1,20 @@
+#include "BackPack.hpp"
+
+namespace BackPackProblem{
+
+	BackPack::BackPack(float maxWeigth){
+		this->maxWeigth = maxWeigth;
+	}
+
+	float BackPack::getMaxWeigth(){
+		return this->maxWeigth;
+	}
+
+	void BackPack::setItems(std::list<Item> items){
+		this->items = items;
+	}
+
+	std::list<Item> BackPack::getItems(){
+		return this->items;
+	}
+}
