f166b509e6b6428f5b5ecac49c79f2cf7249c45a
Pendulun <danielcampos14@hotmail.com.br>
2021-03-27 17:28:17 -0300

    main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "BackPackFactory.hpp"
+#include "BackPackBTFactory.hpp"
+#include "BackPackBBFactory.hpp"
+
+
+int main(int argc, char *argv[]){
+
+	if(argc > 1){
+
+	}else{
+		std::cout<<"Informe o endereco do arquivo de teste!";
+	}
+}
