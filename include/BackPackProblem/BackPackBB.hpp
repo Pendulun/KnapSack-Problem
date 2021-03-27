6db9dd13aa5bbf0444edbee72dfac056bf180856
Pendulun <danielcampos14@hotmail.com.br>
2021-03-27 16:31:59 -0300

    BackPackBB.hpp

--- include/BackPackProblem/BackPackBB.hpp
+++ include/BackPackProblem/BackPackBB.hpp
@@ -1,4 +1,13 @@
 #ifndef BACKPACKBB_H
 #define BACKPACKBB_H
+#include <list>
+#include "BackPack.hpp"
 
+namespace BackPackProblem{
+	class BackPackBB : public BackPack{
+	public:
+		BackPackBB(float maxWeight);
+		float solve() override;
+	};
+}
 #endif
