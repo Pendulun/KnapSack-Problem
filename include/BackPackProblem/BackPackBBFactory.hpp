7f6f6bf8777629d7569edaedc68914fcf1ac1691
Pendulun <danielcampos14@hotmail.com.br>
2021-03-27 16:37:02 -0300

    BackPackBBFactory.hpp

--- include/BackPackProblem/BackPackBBFactory.hpp
+++ include/BackPackProblem/BackPackBBFactory.hpp
@@ -1,4 +1,12 @@
 #ifndef BACKPACKBBFACTORY_H
 #define BACKPACKBBFACTORY_H
+#include "BackPack.hpp"
 
+namespace BackPackProblem{
+	class BackPackBBFactory : public BackPackFactory{
+	public:
+		BackPackBBFactory();
+		BackPack* createBackPack() override;
+	};
+}
 #endif
--- include/BackPackProblem/BackPackFactory.hpp
+++ include/BackPackProblem/BackPackFactory.hpp
@@ -1,4 +1,13 @@
 #ifndef BACKPACKFACTORY_H
 #define BACKPACKFACTORY_H
+#include "BackPack.hpp"
 
+namespace BackPackProblem{
+	class BackPackFactory{
+	public:
+		BackPackFactory();
+		virtual ~BackPackFactory();
+		virtual BackPack* createBackPack() = 0;
+	};
+}
 #endif
