ce12f989c1c05a89c03d9ecf64d5bde1a7d53d2d
Pendulun <danielcampos14@hotmail.com.br>
2021-03-27 21:56:04 -0300

    Melhorias e temporizador

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,81 @@
 #include <iostream>
 #include <fstream>
-#include <list>
+#include <vector>
 #include "BackPackFactory.hpp"
 #include "BackPackBTFactory.hpp"
 #include "BackPackBBFactory.hpp"
 #include "Item.hpp"
+#include <chrono>
 
 int main(int argc, char *argv[]){
 
 	if(argc > 1){
-		std::string entrada = argv[1];
-		std::ifstream arquivoEntrada;
-		arquivoEntrada.open(entrada, std::ios::in);
+		std::string entrada = "";
+		std::ofstream arquivoSaidaBT;
+		arquivoSaidaBT.open("saidaBackTracking.csv", std::ios::app);
+		std::ofstream arquivoSaidaBB;
+		arquivoSaidaBB.open("saidaBranchAndBound.csv", std::ios::app);
 
-		if(arquivoEntrada.is_open()){
-			std::list<BackPackProblem::Item> items;
-			int n = 0;
-			float wMax = 0.0;
-			arquivoEntrada >> n >> wMax;
-			for(int i = 0; i<n;i++){
-				float valor = 0.0;
-				float peso = 0.0;
-				arquivoEntrada >> valor >> peso;
-				BackPackProblem::Item novoItem(valor,peso);
-				items.push_back(novoItem);
-			}
+			for(int i=1;i<argc;i++){
+				entrada = argv[i];
+				std::ifstream arquivoEntrada;
+				arquivoEntrada.open(entrada, std::ios::in);
 
-			for(int i = 0; i<2;i++){
-				BackPackProblem::BackPackFactory* fabricaMochila = nullptr;
-				BackPackProblem::BackPack* mochila = nullptr;
-				std::string saida = "";
-				std::ofstream arquivoSaida;
+				if(arquivoEntrada.is_open() && arquivoSaidaBB.is_open() && arquivoSaidaBT.is_open()){
+					std::vector<BackPackProblem::Item> items;
+					int n = 0;
+					float wMax = 0.0;
+					arquivoEntrada >> n >> wMax;
+					for(int i = 0; i<n;i++){
+						float valor = 0.0;
+						float peso = 0.0;
+						arquivoEntrada >> valor >> peso;
+						BackPackProblem::Item novoItem(peso,valor);
+						items.push_back(novoItem);
+					}
 
-				if(i==0){
-					fabricaMochila = new BackPackProblem::BackPackBTFactory();
-					mochila = fabricaMochila->createBackPack();
-					mochila->setMaxWeigth(wMax);
-					saida = "saidaBackTracking.txt";
+					for(int i = 0; i<2;i++){
+						BackPackProblem::BackPackFactory* fabricaMochila = nullptr;
+						BackPackProblem::BackPack* mochila = nullptr;
+
+						auto start = std::chrono::high_resolution_clock::now();
+				
+						if(i==0){
+							fabricaMochila = new BackPackProblem::BackPackBTFactory();
+							mochila = fabricaMochila->createBackPack();
+							mochila->setMaxWeigth(wMax);
+						}else{
+							fabricaMochila = new BackPackProblem::BackPackBBFactory();
+							mochila = fabricaMochila->createBackPack();
+							mochila->setMaxWeigth(wMax);
+						}
+						auto stop = std::chrono::high_resolution_clock::now();
+						std::chrono::duration<float> duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+						mochila->setItems(items);
+						float resultado = mochila->solve();
+						if(i==0){
+							arquivoSaidaBT<<entrada<<";DanielSouzaDeCampos;"<<duration.count()<<";"<<resultado<<std::endl;
+						}else{
+							arquivoSaidaBB<<entrada<<";DanielSouzaDeCampos;"<<duration.count()<<";"<<resultado<<std::endl;
+						}
+						delete mochila;
+						delete fabricaMochila;
+						
+					}
+
 				}else{
-					fabricaMochila = new BackPackProblem::BackPackBBFactory();
-					mochila = fabricaMochila->createBackPack();
-					mochila->setMaxWeigth(wMax);
-					saida = "saidaBranchAndBound.txt";
-				}
-
-				arquivoSaida.open(saida, std::ios::app);
-				if(arquivoSaida.is_open()){
-					mochila->setItems(items);
-					float resultado = mochila->solve();
-
-					arquivoSaida<<entrada<<";"<<"DanielSouzaDeCampos;tempo;"<<resultado<<"\n"<<std::endl;
-
-					arquivoSaida.close();
-					delete mochila;
-					delete fabricaMochila;
-				}else{
-					std::cout<<"Nao foi possivel abrir o arquivo de saida!";
+					std::cout<<"Nao foi possivel abrir o arquivo. Confira o endereco e nome do arquivo!";
 					return -1;
-				}
-			}
+				}	
 			arquivoEntrada.close();
-		}else{
-			std::cout<<"Nao foi possivel abrir o arquivo. Confira o endereco e nome do arquivo!";
-			return -1;
-		}	
+		}
+		arquivoSaidaBT.close();
+		arquivoSaidaBB.close();
 	}else{
 		std::cout<<"Informe o endereco do arquivo de teste!";
 		return -1;
 	}
+	std::cout<<"Foram gerados dois arquivos com as respostas para cada algoritmo!"<<std::endl;
 	return 0;
 }
