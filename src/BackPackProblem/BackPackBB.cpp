37cccf778943f49d3865e18a967c7cb949d3cb01
Pendulun <danielcampos14@hotmail.com.br>
2021-03-28 20:09:09 -0300

    BranchAndBound implementation

--- src/BackPackProblem/BackPackBB.cpp
+++ src/BackPackProblem/BackPackBB.cpp
@@ -34,12 +34,58 @@ namespace BackPackProblem{
 		return limiteSuperior;
 	}
 
-	//Não implementado
 	float BackPackBB::maiorValorPossivel(){
 		
 		std::queue<BackPackProblem::Node> filaRamos;
 
-		
+		BackPackProblem::Node noAtual;
+		BackPackProblem::Node proxNo;
+
+		float resultado = 0.0;
+
+		noAtual.setIndex(-1);
+		noAtual.setValue(0.0);
+		noAtual.setWeigth(0.0);
+
+		filaRamos.push(noAtual);
+
+		while(!filaRamos.empty()){
+			noAtual = filaRamos.front();
+			filaRamos.pop();
+
+			if(noAtual.getIndex() == -1){
+				proxNo.setIndex(0);
+			}
+
+			if(noAtual.getIndex() == this->getItems().size() -1){
+				continue;
+			}
+
+			proxNo.setIndex(noAtual.getIndex()+1);
+			proxNo.setWeigth(noAtual.getWeigth() + this->getItems()[proxNo.getIndex()].getWeigth());
+			proxNo.setValue(noAtual.getValue() + this->getItems()[proxNo.getIndex()].getValue());
+
+			if(proxNo.getWeigth() <= this->getMaxWeigth() && proxNo.getValue() > resultado){
+				resultado = proxNo.getValue();
+			}
+
+			proxNo.setBound(this->calculaUB(proxNo));
+
+			if(proxNo.getBound() > resultado){
+				filaRamos.push(proxNo);
+			}
+
+			proxNo.setWeigth(noAtual.getWeigth());
+			proxNo.setValue(noAtual.getValue());
+			proxNo.setBound(this->calculaUB(proxNo));
+
+			if(proxNo.getBound() > resultado){
+				filaRamos.push(proxNo);
+			}
+
+		}
+
+		return resultado;
 	}
 
 	float BackPackBB::solve(){
