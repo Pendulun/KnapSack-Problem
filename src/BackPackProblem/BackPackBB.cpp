ddf4d8ceb340d85ef8141496f8130f60e53779f0
Pendulun <danielcampos14@hotmail.com.br>
2021-03-28 19:35:56 -0300

    calcula UB implementação

--- src/BackPackProblem/BackPackBB.cpp
+++ src/BackPackProblem/BackPackBB.cpp
@@ -8,15 +8,38 @@ namespace BackPackProblem{
 	BackPackBB::~BackPackBB(){
 	}
 
-	float BackPackBB::calculaUB(const Item &proxItem, float valor, float pesoRestante){
-		float valorEstimado = proxItem.getValue();
-		valorEstimado /= proxItem.getWeigth();
-		return valor + pesoRestante*valorEstimado;
+	float BackPackBB::calculaUB(BackPackProblem::Node& noAtual){
+		if(noAtual.getWeigth() > this->getMaxWeigth()){
+			return 0;
+		}
+
+		float limiteSuperior = noAtual.getValue();
+
+		int proxIndex = noAtual.getIndex() + 1;
+
+		float pesoTotalUsado = noAtual.getWeigth();
+
+		while((proxIndex < this->getItems().size()) && (pesoTotalUsado + this->getItems()[proxIndex].getWeigth() <= this->getMaxWeigth())){
+			pesoTotalUsado += this->getItems()[proxIndex].getWeigth();
+			limiteSuperior += this->getItems()[proxIndex].getValue();
+			proxIndex++;
+		}
+
+		if(proxIndex < this->getItems().size()){
+			float pesoRestante = this->getMaxWeigth() - pesoTotalUsado;
+			float valorRelativo = this->getItems()[proxIndex].getValue()/this->getItems()[proxIndex].getWeigth();
+			limiteSuperior += (pesoRestante) * (valorRelativo);
+		}
+
+		return limiteSuperior;
 	}
 
 	//Não implementado
 	float BackPackBB::maiorValorPossivel(){
-		return -1;
+		
+		std::queue<BackPackProblem::Node> filaRamos;
+
+		
 	}
 
 	float BackPackBB::solve(){
