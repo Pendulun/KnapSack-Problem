6d8111c181471a13ba5d47e1e26e037636fa4048
Pendulun <danielcampos14@hotmail.com.br>
2021-03-28 00:28:29 -0300

    primeira versão do BB

--- src/BackPackProblem/BackPackBB.cpp
+++ src/BackPackProblem/BackPackBB.cpp
@@ -9,10 +9,32 @@ namespace BackPackProblem{
 	}
 
 	float BackPackBB::maiorValorPossivel(){
-		return -1;
+		if(itemIndex == this->BackPack::getItems().size()){
+			return valorTotal;
+		}else{
+
+			float pesoItem = this->BackPack::getItems()[itemIndex].getWeigth();
+			float valorItem = this->BackPack::getItems()[itemIndex].getValue();
+			float valorComItem = 0.0;
+			float valorSemItem = 0.0;
+
+			//Poda de ramo
+			if(pesoRestante-pesoItem >=0){
+				valorComItem = this->maiorValorPossivel(itemIndex+1,pesoRestante-pesoItem,valorTotal+valorItem);
+			}
+
+			valorSemItem = this->maiorValorPossivel(itemIndex+1,pesoRestante,valorTotal);
+
+			if(valorComItem > valorSemItem){
+				return valorComItem;
+			}else{
+				return valorSemItem;
+			}
+		}
 	}
 
 	float BackPackBB::solve(){
+		
 		return maiorValorPossivel();
 	}
 }
